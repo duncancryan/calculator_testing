e157f22cf21b96e2b985532282b1c64db5adeabd
Duncan Ryan <duncancryan1@gmail.com>
2020-10-11 12:59:33 +0100

    Initial Commit - add() test passing

---
+++ tests/unit/calculator.spec.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue'
+
+describe('App.vue', () => {
+  it('enterNum changes running total', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 4
+    wrapper.vm.add('5');
+    expect(wrapper.vm.runningTotal).to.equal(9)
+  })
+
+  it('can add two numbers', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 1
+    wrapper.vm.add('4');
+    expect(wrapper.vm.runningTotal).to.equal(5)
+  })
+})
