395b9857267c4ef2786c68f10b2e9cc04ccc707a
Duncan Ryan <duncancryan1@gmail.com>
2020-10-11 14:29:37 +0100

    Number buttons are updating running total display on the front end

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -9,4 +9,11 @@ describe('calculator', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should update the display of the running total when a number button is clicked', () => {
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '2')
+    cy.get('#number4').click();
+    cy.get('.display').should('contain', '24')
+  })
 })
